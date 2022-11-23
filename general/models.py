2a3f6a3f3efa21f62d0c7187068462394a3f255d
Le Nguyen Khang <lenguyenkhang21112003@gmail.com>
2022-11-23 15:49:27 +0700

    Add Course model and Category model

--- general/models.py
+++ general/models.py
@@ -9,3 +9,17 @@ class Customer(models.Model):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    best_seller_course = models.ForeignKey(
+        'Course', on_delete=models.SET_NULL, null=True)
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2,
+                                validators=[MinValueValidator(1)])
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
