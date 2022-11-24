f23e7efaf6a046a72d4251157df8b5fc41b590ea
Le Nguyen Khang <lenguyenkhang21112003@gmail.com>
2022-11-24 08:13:10 +0700

    Delete general app and divides its model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    best_seller_course = models.ForeignKey(
+        'Course', on_delete=models.SET_NULL, null=True, related_name='best_seller')
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2,
+                                validators=[MinValueValidator(1)])
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
