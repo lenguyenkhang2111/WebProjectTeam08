78e24f730c24d94cbc961962b53dde9aef2b8edd
Le Nguyen Khang <lenguyenkhang21112003@gmail.com>
2022-11-26 14:40:54 +0700

    Add AccountManager models

--- account/models.py
+++ account/models.py
@@ -1,10 +1,48 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError('Email address is required')
+
+        if not username:
+            raise ValueError('User name is required')
+
+        # Tạo đối tượng user mới
+        user = self.model(
+            # Chuyển email về dạng bình thường
+            email=self.normalize_email(email=email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, email, username, password):
+        user = self.create_user(
+            email=self.normalize_email(email=email),
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
 
 
 class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
@@ -13,7 +51,6 @@ class Account(AbstractBaseUser):
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now_add=True)
     is_admin = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
     is_superadmin = models.BooleanField(default=False)
 
