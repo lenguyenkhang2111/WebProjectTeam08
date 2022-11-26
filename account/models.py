d2d577123a3092886f06edf10faab79ed62bad0f
Le Nguyen Khang <lenguyenkhang21112003@gmail.com>
2022-11-26 14:35:47 +0700

    Add some field

--- account/models.py
+++ account/models.py
@@ -9,9 +9,13 @@ class Account(AbstractBaseUser):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
     subscription_expired = models.DateField(null=True, blank=True)
-    USERNAME_FIELD = 'email'    # Trường quyêt định khi login
-    # Các trường yêu cầu khi đk tài khoản (mặc định đã có email), mặc định có password
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    # REQUIRED_FIELDS
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.email
