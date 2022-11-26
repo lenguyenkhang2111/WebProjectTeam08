cf7f9b8ce398390ccfbd326fa16bf15f54bd1faf
Le Nguyen Khang <lenguyenkhang21112003@gmail.com>
2022-11-26 14:24:42 +0700

    Add new fields to Account model

--- account/models.py
+++ account/models.py
@@ -1,10 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 
-class Account(models.Model):
+class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
     subscription_expired = models.DateField(null=True, blank=True)
+    USERNAME_FIELD = 'email'    # Trường quyêt định khi login
+    # Các trường yêu cầu khi đk tài khoản (mặc định đã có email), mặc định có password
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin    # Admin có tất cả quyền trong hệ thống
+
+    def has_module_perms(self, add_label):
+        return True
+
+    def full_name(self):
+        return self.first_name + " " + self.last_name
