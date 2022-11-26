a58f28de8e47cc87076dbd0eb1074eb5da2aaa89
Le Nguyen Khang <lenguyenkhang21112003@gmail.com>
2022-11-26 16:12:11 +0700

    Merge code second

--- WebProjectTeam08/urls.py
+++ WebProjectTeam08/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('product/', include('product.urls')),
-    path('account/', include('accounts.urls'))
+    path('account/', include('account.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
